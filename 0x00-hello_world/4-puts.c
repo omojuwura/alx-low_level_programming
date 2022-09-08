e5477d3e728b86ead1402a30dc3552086bf2a7b1
omojuwura <omowumijuwura@gmail.com>
2022-09-08 09:33:20 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,7 @@
 *Return: 0 after printing the function
 */
 int main(void)
-{puts("\"Programming is like building a multiligual puzzle");
-        return (0);
 {puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+ls
