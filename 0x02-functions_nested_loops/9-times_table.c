c69dde8f0c411bde98fd011dd8a1e365b50eabb5
omojuwura <omowumijuwura@gmail.com>
2022-09-13 15:58:13 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,18 +3,19 @@
 /**
  * times_table - this prints the 9 times table, starts wiith the zero
  */
-void times_table (void)
+void times_table(void)
 {
 	int num, mult, prod;
-	for (num = 0; num <=9; num++)
+
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
-		for (mult = 1; mult <=9; mult++)
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
+
 			prod = num * mult;
-			
 			if (prod <= 9)
 				_putchar(' ');
 			else
