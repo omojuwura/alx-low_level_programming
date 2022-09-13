e6b552e7dae1f75d46c5c8ea7d6bcdc6d6afc108
omojuwura <omowumijuwura@gmail.com>
2022-09-13 16:22:44 -0700

    print pls

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints numbers inputed from 98, in a oredr followed by a comma and space
+ * @n: start the counting from here
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+				printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
+
