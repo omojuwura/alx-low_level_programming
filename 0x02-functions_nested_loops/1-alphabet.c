49bf6d3173b7154145ff8e7fe90804de2939fff1
omojuwura <omowumijuwura@gmail.com>
2022-09-13 17:30:36 -0700

    oshey o

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - function to print abc 
+ * print_alphabet - function to print abc 
  *
  * Return: 0
  */
@@ -10,7 +10,7 @@ void print_alphabet(void)
 
 	for(c = 'a'; c <= 'z'; c++)
 	{
-		putchar(c);
+		_putchar(c);
 	}
 _putchar('\n');
 }
