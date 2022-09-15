7ab492a1cb4cf81fc48d53a21a95d41b8d772ad5
omojuwura <omowumijuwura@gmail.com>
2022-09-14 23:46:55 -0700

    go now

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 int inc;
-unsigned long n1 =0, n2 =1, n3;
+unsigned long n1 = 0, n2 = 1, n3;
 for (inc = 0; inc < 50; inc++)
 {
 n3 = n1 + n2;
@@ -18,7 +18,7 @@ n2 = n3;
 if (inc == 49)
 printf("\n");
 else
-printf(",");
+printf(", ");
 
 }
 
