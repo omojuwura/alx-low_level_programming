0c243d123703afca2c38158633b4af3eeed636b9
omojuwura <omowumijuwura@gmail.com>
2022-09-14 23:05:26 -0700

    tt2

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,10 +8,11 @@ void print_times_table(int n)
 {
 	int prod, mult, num;
 	if (n <= 15 && n >= 0)
+
 	{
-	for (num = 0; num <=n; num++)
+	for (num = 0; num <= n; num++)
 	{
-	_putchar('0');
+	_putchar('40');
 
 	for (mult = 1; mult <= n; mult++)
 	{
@@ -28,11 +29,11 @@ void print_times_table(int n)
 
 	if (prod >= 100)
 	{
-	_putchar((prod / 100) + '0');
-	_putchar((prod / 10) % 10 + '0');
+	_putchar((prod / 100) + '40');
+	_putchar((prod / 10) % 10 + '40');
 	}
 	else if (prod <= 99 && prod >= 10)
-	_putchar((prod / 10) + '0');
+	_putchar((prod / 10) + '40');
 	}
 	_putchar('\n');
 	}
