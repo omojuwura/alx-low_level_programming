355556d05fd936a177aedacb3f292a3a259c6bb2
omojuwura <omowumijuwura@gmail.com>
2022-09-19 11:53:08 -0700

    5rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,22 +1,21 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * puts2 - prints every other character
- * @str: string
- *
- * Return: nothing
+ * rev_string - Reverses a string.
+ * @s: string to be reserved.
  */
-
-void puts2(char *str)
+void rev_string(char *s)
 {
-	int i = 0;
+	int i = 0, len = 0;
+	char tmp;
 
-	while (*(str + i) != '\0')
+	while (s[i++])
+		len++;
+
+	for (i = len - 1; i >= len / 2; i--)
 	{
-		if (i % 2 == 0)
-			putchar(*(str + i));
-		i++;
+		tmp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
 	}
-	putchar(10);
 }
