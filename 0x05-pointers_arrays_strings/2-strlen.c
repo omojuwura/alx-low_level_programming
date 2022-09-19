a4794831a4bf8350751581f5c6829e30993e7e2d
omojuwura <omowumijuwura@gmail.com>
2022-09-19 10:30:53 -0700

    strln

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,12 +6,13 @@
  *
  * Return: Length.
  */
-size_t _strlen(const char *str)
+
+int _strlen(char *s)
 {
-	size_t len = 0;
+        int len = 0;
 
-	while (*str++)
-		len++;
+        while (*(s + len) != '\0')
+                len++;
 
-	return (len);
+        return (len);
 }
