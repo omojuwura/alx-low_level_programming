67de9f3f8c8abffd7bd6e5108ec463c722496d71
omojuwura <omowumijuwura@gmail.com>
2022-09-30 18:04:36 -0700

    i

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a char in a string
+ * @s: string to be searched
+ * @c: char to be checked
+ *
+ * Return: pointer to the first occurence of c in s
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i); i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
+
+	if (*(s + i) == c)
+		return (s + i);
+	return (0);
+}
