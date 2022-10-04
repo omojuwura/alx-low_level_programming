eb55e615e4e4ab5dcf17b6627766b8643eba7f98
omojuwura <omowumijuwura@gmail.com>
2022-10-03 23:14:37 -0700

    push

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,12 +1,10 @@
 #include "main.h"
-#include <stdlib.h>
-
 /**
- * free_grid - frees a 2-D grid previously created by alloc_grid
- * @grid: pointer to grid to free
- * @height: height of grid
+ * free_grid -> removing memory allocation
+ * @grid: grid to be freed
+ * @height: height of matrix
+ * Return: nothing
  */
-
 void free_grid(int **grid, int height)
 {
 	int i;
