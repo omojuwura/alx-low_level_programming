bbd00c43b210285afc74635057da8b5685614535
omojuwura <omowumijuwura@gmail.com>
2022-10-12 00:39:32 -0700

    push

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,12 @@
-#ifndef DOG_H
-#define DOG_H
-
+#ifndef DOG
+#define DOG
 /**
- * struct dog - Struct dog
- * @name: name
- * @age: age
- * @owner:  owner
- * Description: This struct is for dog
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * Description: Longer description
  */
-
 struct dog
 {
 	char *name;
@@ -16,10 +14,14 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - Typedef for struct dog
+ */
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /*DOG_H*/
+#endif
