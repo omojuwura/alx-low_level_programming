7653473f5e14142a3c009809c0a8afaec2c0d9bf
omojuwura <omowumijuwura@gmail.com>
2023-01-06 19:50:21 -0800

    hashtables

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -6,11 +6,13 @@ char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
+
 /**
  * shash_table_create - Creates a sorted hash table.
  * @size: The size of new sorted hash table.
+ *
  * Return: If an error occurs - NULL.
- *	Otherwise - a pointer to the new sorted hash table.
+ *         Otherwise - a pointer to the new sorted hash table.
  */
 shash_table_t *shash_table_create(unsigned long int size)
 {
@@ -26,10 +28,9 @@ shash_table_t *shash_table_create(unsigned long int size)
 	if (ht->array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
-		ht->array[i] = NULL
-			ht->shead = NULL;
+		ht->array[i] = NULL;
+	ht->shead = NULL;
 	ht->stail = NULL;
-
 
 	return (ht);
 }
@@ -39,6 +40,7 @@ shash_table_t *shash_table_create(unsigned long int size)
  * @ht: A pointer to the sorted hash table.
  * @key: The key to add - cannot be an empty string.
  * @value: The value associated with key.
+ *
  * Return: Upon failure - 0.
  *         Otherwise - 1.
  */
@@ -121,6 +123,7 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
  *                   a key in a sorted hash table.
  * @ht: A pointer to the sorted hash table.
  * @key: The key to get the value of.
+ *
  * Return: If the key cannot be matched - NULL.
  *         Otherwise - the value associated with key in ht.
  */
@@ -170,7 +173,6 @@ void shash_table_print(const shash_table_t *ht)
  * shash_table_print_rev - Prints a sorted hash table in reverse order.
  * @ht: A pointer to the sorted hash table to print.
  */
-
 void shash_table_print_rev(const shash_table_t *ht)
 {
 	shash_node_t *node;
